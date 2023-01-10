e67e0ced1c3c3ee563affd6eb5cff51939da34c7
ngankedd <necrogankedd@gmail.com>
2023-01-10 15:53:13 -0800

    created new maps

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1059,3 +1059,13 @@ Common_EventScript_LegendaryFlewAway::
 	.include "data/text/frontier_brain.inc"
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
+
+	.include "data/maps/PalletTown/scripts.inc"
+
+	.include "data/maps/Route1/scripts.inc"
+
+	.include "data/maps/Route21_North/scripts.inc"
+
+	.include "data/maps/ViridianCity/scripts.inc"
+
+	.include "data/maps/Route22/scripts.inc"
